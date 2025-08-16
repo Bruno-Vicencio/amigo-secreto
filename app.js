f67c493463b4aff9c44795b5fcf87c1a4032ce4e
Bruno Vicencio <brunovicencio430@gmail.com>
2025-08-16 12:17:15 -0400

    Creacion funcion agregarAmigo + validar nombre + limpiar campo.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Lista donde guardaremos los nombres
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, escribe un nombre válido.");
+        return;
+    }
+
+    //Validar si el nombre ya existe.
+    if (amigos.some(a => a.toLowerCase() === nombre.toLowerCase())) {
+    alert('Ese nombre ya está en la lista.');
+    return;
+}
+
+    amigos.push(nombre);
+    input.value = ""; // Limpiar campo
+    mostrarLista();
+}
+
